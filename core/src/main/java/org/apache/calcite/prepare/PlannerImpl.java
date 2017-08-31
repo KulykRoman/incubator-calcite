vkorukanti/optiq

Add new method to ViewExpander interface to allow passing SchemaRoot.

core/src/main/java/net/hydromatic/optiq/prepare/PlannerImpl.java
@@ -199,6 +199,31 @@ public class PlannerImpl implements Planner {
   public class ViewExpanderImpl implements ViewExpander {
     public RelNode expandView(RelDataType rowType, String queryString,
         List<String> schemaPath) {
+      final OptiqCatalogReader catalogReader =
+          createCatalogReader().withSchemaPath(schemaPath);
+
+      return expandViewHelper(queryString, catalogReader);
+    }
+
+    public RelNode expandView(
+        RelDataType rowType,
+        String queryString,
+        SchemaPlus rootSchema,
+        List<String> schemaPath) {
+
+      // View may have different schema path than current connection.
+      final OptiqCatalogReader catalogReader = new OptiqCatalogReader(
+          OptiqSchema.from(rootSchema),
+          parserConfig.caseSensitive(),
+          OptiqSchema.from(defaultSchema).path(null),
+          typeFactory).withSchemaPath(schemaPath);
+
+      return expandViewHelper(queryString, catalogReader);
+    }
+
+    private RelNode expandViewHelper(
+        String queryString,
+        OptiqCatalogReader catalogReader) {
       SqlParser parser = SqlParser.create(parserFactory, queryString,
           parserConfig);
       SqlNode sqlNode;
@@ -208,8 +233,6 @@ public class PlannerImpl implements Planner {
         throw new RuntimeException("parse failed", e);
       }
 
-      final OptiqCatalogReader catalogReader =
-          createCatalogReader().withSchemaPath(schemaPath);
       SqlValidator validator = createSqlValidator(catalogReader);
       validator.setIdentifierExpansion(true);
       SqlNode validatedSqlNode = validator.validate(sqlNode);

KulykRoman/incubator-calcite

Conflicts:
	core/src/main/java/org/apache/calcite/prepare/PlannerImpl.java
	core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java

core/src/main/java/org/apache/calcite/prepare/PlannerImpl.java
@@ -241,6 +241,23 @@ public class PlannerImpl implements Planner {
   public class ViewExpanderImpl implements ViewExpander {
     @Override public RelRoot expandView(RelDataType rowType, String queryString,
       List<String> schemaPath, List<String> viewPath) {
+      final CalciteCatalogReader catalogReader =
+          createCatalogReader().withSchemaPath(schemaPath);
+      return expandViewHelper(queryString, catalogReader);
+    }
+
+    @Override public RelRoot expandView(RelDataType rowType, String queryString,
+      SchemaPlus rootSchema, List<String> schemaPath) {
+      // View may have different schema path than current connection.
+      final CalciteCatalogReader catalogReader = new CalciteCatalogReader(
+          CalciteSchema.from(rootSchema),
+          parserConfig.caseSensitive(),
+          CalciteSchema.from(defaultSchema).path(null),
+          typeFactory).withSchemaPath(schemaPath);
+      return expandViewHelper(queryString, catalogReader);
+    }
+
+    private RelRoot expandViewHelper(String queryString, CalciteCatalogReader catalogReader) {
       SqlParser parser = SqlParser.create(queryString, parserConfig);
       SqlNode sqlNode;
       try {
@@ -250,8 +267,6 @@ public class PlannerImpl implements Planner {
       }
 
       final SqlConformance conformance = conformance();
-      final CalciteCatalogReader catalogReader =
-          createCatalogReader().withSchemaPath(schemaPath);
       final SqlValidator validator =
           new CalciteSqlValidator(operatorTable, catalogReader, typeFactory,
               conformance);